8e0ce7e86d8f3a48eb37621d857f79cab04f2c06
EN <iwagner76@gmail.com>
2023-01-31 11:40:29 -0500

    set up

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,17 @@
 require("dotenv").config;
 const { DISCORD_TOKEN } = process.env;
-const { Client, Collection, GatewayIntentBits } = require("discord.js");
+import { Client, Collection, GatewayIntentBits, ClientUser } from "discord.js";
+import { readdirSync } from "fs";
+
+const client = Client({ intents: GatewayIntentBits.Guilds });
+client.commands = new Collection();
+client.commandArray = [];
+
+const functionFolders = readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+  const functionFiles = readdirSync(`src/function/${folder}`).filter((file) =>
+    file.endsWith(".js")
+  );
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
+}
---
+++ src/functions/handlers/handleEvents.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+
+module.exports = (client) => {
+  client.handleEvents = async () => {
+    const eventFolder = fs.readdirSync("./src/events");
+    for (const folder of eventFolders) {
+      const eventFiles = fs
+        .readdirSync(`./src/event/${folder}`)
+        .filter((file) => endsWith(".js"));
+      switch (folder) {
+        case "client":
+          for (const file of eventFiles) {
+            const event = require(`../../events/${folder}/${file}`);
+            if (event.once)
+              client.once(event.name, (...args) =>
+                event.execute(...args, client)
+              );
+            else
+              client.on(event.name, (...args) =>
+                event.execute(...args, client)
+              );
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+  };
+};
