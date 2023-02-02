648ee43b3294b8a8dee4fd5c209c03659d633f26
EN <iwagner76@gmail.com>
2023-02-01 20:26:46 -0500

    fixed discord error with deferReply

--- src/commands/tools/aiPrompt.js
+++ src/commands/tools/aiPrompt.js
@@ -14,19 +14,18 @@ module.exports = {
       option.setName("prompt").setDescription("Prompt").setRequired(true)
     ),
   async execute(interaction) {
+    await interaction.deferReply({
+      fetchReply: true,
+    });
     const prompt = interaction.options.getString("prompt");
     const response = await openai.createCompletion({
-      model: "text-davinci-002",
+      model: "text-davinci-003",
       prompt: prompt,
-      temperature: 0.7,
+      temperature: 0.9,
       max_tokens: 256,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
-    });
-
-    await interaction.deferReply({
-      fetchReply: true,
     });
 
     const answer = response.data.choices[0].text;
